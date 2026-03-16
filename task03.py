10199aafea516340a4257982cc3ddddf89e2905a
Jasur <uktamovjasur137@gmail.com>
2026-03-16 22:39:31 +0500

    done

--- task03.py
+++ task03.py
@@ -1,8 +1,8 @@
 n = int(input("n: "))
 
 if n < 15:
-    for i in range(n, 15, 1):
+    for i in range(n, 15 + 1, 1):
         print(i)
 elif n > 15:
-    for i in range(n, 15, -1):
+    for i in range(n, 15 + 1, -1):
         print(i)
